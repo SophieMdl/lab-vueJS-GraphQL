654e66c4430725f1e9ab061d8d1958c21e19daf7
SophieMdl <sophie.michaudel@gmail.com>
2020-01-31 14:29:36 +0100

    init input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            countries: [],
+            search : ""
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
