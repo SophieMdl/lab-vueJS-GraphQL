33ca8679adf9efd3648ef00bfda562149afc5fb2
sophieMdl <sophie.michaudel@gmail.com>
2020-02-19 18:13:24 +0100

    add express server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const graphql = require("graphql");
+const { query } = require(path.resolve("schema/query"));
+const graphQLSchema = new graphql.GraphQLSchema({ query });
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphQLSchema,
+    graphiql: true
+  })
+);
+app.listen(5000);
